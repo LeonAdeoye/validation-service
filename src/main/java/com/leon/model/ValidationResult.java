d2abc902c479e9333607813a932aa1749e72328b
LeonAdeoye <leon.adeoye@gmail.com>
2021-06-23 21:59:34 +0800

    initial commit

--- src/main/java/com/leon/model/ValidationResult.java
+++ src/main/java/com/leon/model/ValidationResult.java
@@ -1,5 +1,34 @@
 package com.leon.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+enum ResultType
+{
+    SUCCESS,
+    FAILURE
+}
+
 public class ValidationResult
 {
+    private List<String> errors;
+    private ResultType result;
+
+    ValidationResult()
+    {
+        errors = new ArrayList<>();
+    }
+
+    String toString()
+    {
+        if(errors.size() != 0)
+        {
+
+        }
+        else
+            result = ResultType.SUCCESS
+
+        return "";
+    }
+
 }
