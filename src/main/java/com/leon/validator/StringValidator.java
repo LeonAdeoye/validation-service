2bbf1f4cb5b1565844dfc49238c1e438424703ff
LeonAdeoye <leon.adeoye@gmail.com>
2021-06-30 21:25:11 +0800

    string case validation

--- src/main/java/com/leon/validator/StringValidator.java
+++ src/main/java/com/leon/validator/StringValidator.java
@@ -2,11 +2,28 @@ package com.leon.validator;
 
 import com.leon.model.FieldValidation;
 
+import java.util.regex.Pattern;
+
 public class StringValidator implements Validator
 {
+    private Pattern uppercasePattern = Pattern.compile("^[0-9A-Z\\W]*$");
+    private Pattern lowercasePattern = Pattern.compile("^[0-9a-z\\W]*$");
+
     @Override
     public boolean validate(String input, FieldValidation fieldValidation)
     {
-        return fieldValidation.getType().equalsIgnoreCase(("STRING"));
+        String format = fieldValidation.getStringFormat();
+        if(format == null || format.isEmpty())
+            return true;
+
+        switch(format.toUpperCase())
+        {
+            case "UPPERCASE":
+                return uppercasePattern.matcher(input).matches();
+            case "LOWERCASE":
+                return lowercasePattern.matcher(input).matches();
+            default:
+                return false;
+        }
     }
 }
