df0ff312a6ab6d57e4e293a49399d5fb397c8a85
LeonAdeoye <leon.adeoye@gmail.com>
2021-07-04 10:50:53 +0800

    double validator test cases

--- src/main/java/com/leon/validator/DoubleValidator.java
+++ src/main/java/com/leon/validator/DoubleValidator.java
@@ -9,9 +9,9 @@ public class DoubleValidator implements Validator
     @Override
     public String validate(String input, FieldValidation fieldValidation)
     {
-        if(input.matches("/^[0-9]+(\\.[0-9]+)?$"))
+        if(input.matches("^[0-9]+(\\.[0-9]+)?$"))
             return "";
         else
-            return String.format("The field value [%s] is not a valid double.", input);
+            return String.format("The field value [%s] is not a valid double value.", input);
     }
 }
