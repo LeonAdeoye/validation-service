acddc8627a92b17dc7558596537c592625af9fee
LeonAdeoye <leon.adeoye@gmail.com>
2021-08-02 22:28:52 +0800

    check for duplicates

--- src/test/java/com/leon/validator/ValidationServiceTest.java
+++ src/test/java/com/leon/validator/ValidationServiceTest.java
@@ -4,6 +4,7 @@ import com.leon.model.FieldValidation;
 import com.leon.model.ValidationConfiguration;
 import com.leon.model.ValidationResult;
 import com.leon.service.ValidationService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +24,11 @@ public class ValidationServiceTest
 {
     @Autowired
     private ValidationService validationService;
+    private String testFilePath;
+    private ValidationConfiguration validationConfiguration = new ValidationConfiguration();
 
-    @Test
-    public void validate_duplicates_not_allowed() throws Exception
+    @Before
+    public void before() throws Exception
     {
         FieldValidation firstFieldValidation = new FieldValidation();
         firstFieldValidation.setId(1);
@@ -39,8 +42,6 @@ public class ValidationServiceTest
         secondFieldValidation.setCanBeEmpty(false);
         secondFieldValidation.setType(Validator.STRING);
 
-        // Arrange
-        ValidationConfiguration validationConfiguration = new ValidationConfiguration();
         validationConfiguration.setDelimiter(",");
         validationConfiguration.setRowStart(1);
         validationConfiguration.setDuplicatesAllowed(false);
@@ -48,37 +49,26 @@ public class ValidationServiceTest
         URL resource = getClass().getClassLoader().getResource("test-data.csv");
         if (resource == null)
             throw new IllegalArgumentException("test-data.csv file not found.");
+        testFilePath = resource.toURI().getRawPath();
+    }
+
+    @Test
+    public void validate_duplicates_not_allowed()
+    {
+        // Arrange
+        validationConfiguration.setDuplicatesAllowed(false);
         // Act
-        ValidationResult actualResult = validationService.validate(resource.toURI().getRawPath(), validationConfiguration);
+        ValidationResult actualResult = validationService.validate(testFilePath, validationConfiguration);
         // Assert
         assertEquals("Validation that duplicates are not allowed",  Arrays.asList("The row already exists: DataRow{rowNumber=4, rowValues=[Harper, 1]}"), actualResult.getErrors());
     }
     @Test
     public void validate_duplicates_allowed() throws Exception
     {
-        FieldValidation firstFieldValidation = new FieldValidation();
-        firstFieldValidation.setId(1);
-        firstFieldValidation.setDescription("Name");
-        firstFieldValidation.setCanBeEmpty(false);
-        firstFieldValidation.setType(Validator.STRING);
-
-        FieldValidation secondFieldValidation = new FieldValidation();
-        secondFieldValidation.setId(2);
-        secondFieldValidation.setDescription("Wealth");
-        secondFieldValidation.setCanBeEmpty(false);
-        secondFieldValidation.setType(Validator.STRING);
-
-        // Arrange
-        ValidationConfiguration validationConfiguration = new ValidationConfiguration();
-        validationConfiguration.setDelimiter(",");
-        validationConfiguration.setRowStart(1);
+        //Arrange
         validationConfiguration.setDuplicatesAllowed(true);
-        validationConfiguration.setListOfFieldValidations(Arrays.asList(firstFieldValidation, secondFieldValidation));
-        URL resource = getClass().getClassLoader().getResource("test-data.csv");
-        if (resource == null)
-            throw new IllegalArgumentException("test-data.csv file not found.");
         // Act
-        ValidationResult actualResult = validationService.validate(resource.toURI().getRawPath(), validationConfiguration);
+        ValidationResult actualResult = validationService.validate(testFilePath, validationConfiguration);
         // Assert
         assertEquals("Validation that duplicates are allowed",  Arrays.asList(), actualResult.getErrors());
     }
